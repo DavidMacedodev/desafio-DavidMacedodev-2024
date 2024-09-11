d25bd39efb4fafa2af9bf38a41dd6a3a4a5fd9df
David Macedo <davidmacedo.dev@gmail.com>
2024-09-11 15:15:04 -0300

    feat/add animais info

---
+++ src/animaisInfo.js
@@ -0,0 +1,34 @@
+const ANIMAIS_INFO = {
+    "MACACO": {
+        eCarnivoro: false,
+        tamanho: 1,
+        biomas: ["savana", "floresta"]
+    },
+    "HIPOPOTAMO": {
+        eCarnivoro: false,
+        tamanho: 4,
+        biomas: ["savana", "rio"]
+    },
+    "LEOPARDO": {
+        eCarnivoro: true,
+        tamanho: 2,
+        biomas: ["savana"]
+    },
+    "LEAO": {
+        eCarnivoro: true,
+        tamanho: 3,
+        biomas: ["savana"]
+    },
+    "CROCODILO": {
+        eCarnivoro: true,
+        tamanho: 3,
+        biomas: ["rio"]
+    },
+    "GAZELA": {
+        eCarnivoro: false,
+        tamanho: 2,
+        biomas: ["savana"]
+    }
+};
+
+export { ANIMAIS_INFO as ANIMAIS_INFO };
